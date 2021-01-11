337c78a3fe15802642698552f98785ea12e52ad4
sinashk78 <sina.sh.k78@gmail.com>
2021-01-11 11:08:14 +0330

    fixed a bug in energy calculation

--- main.c
+++ main.c
@@ -26,7 +26,7 @@ typedef struct pixel3
 typedef struct fext
 {
   float val;
-  size_t from;
+  int from;
 } fext_t;
 
 // TODO parse argument https://www.gnu.org/software/libc/manual/html_node/Argp.html
@@ -64,7 +64,22 @@ int main(int argc, char const *argv[])
     return 1;
   }
 
+  pixel3_t *temp = pixels;
+  for (size_t i = 0; i < h; i++)
+  {
+    for (size_t j = 0; j < w; j++)
+    {
+      printf("(%d,%d,%d)\t", temp[idx(i, j, w)].r, temp[idx(i, j, w)].g, temp[idx(i, j, w)].b);
+    }
+    printf("\n");
+  }
+  printf("\n");
+  printf("\n");
+  struct timespec s, e;
+  clock_gettime(CLOCK_REALTIME, &s);
   resize_image(pixels, w, h, 0, 0);
+  clock_gettime(CLOCK_REALTIME, &e);
+  printf("%lf\n", e.tv_sec - s.tv_sec + (e.tv_nsec - s.tv_nsec) / pow(10, 9));
 
   // calculate seam
   printf("w: %d h: %d ch: %d\n", w, h, ch);
@@ -83,8 +98,14 @@ void resize_image(unsigned char *pixels, int w, int h, int target_width, int tar
   // calculate energy
   pixel3_t *energy_img = malloc(w * h * sizeof(*energy_img));
   float *e = calc_energy3((pixel3_t *)pixels, w, h, energy_img);
-  fext_t m[h][w];
   int i, j;
+  fext_t **m = malloc(h * sizeof(*m));
+
+  for (i = 0; i < h; i++)
+  {
+    m[i] = malloc(w * sizeof(*m));
+  }
+
   float min = FLT_MAX;
 
   for (i = 0; i < h; i++)
@@ -130,7 +151,7 @@ void resize_image(unsigned char *pixels, int w, int h, int target_width, int tar
   {
     for (j = 0; j < h; j++)
     {
-      printf("%ld\t", m[i][j].from);
+      printf("%d\t", m[i][j].from);
     }
     printf("\n");
   }
@@ -177,16 +198,17 @@ float *calc_energy3(pixel3_t *pixels, int w, int h, pixel3_t *energy_img)
 
   float *energies = malloc(w * h * sizeof(float));
 
-  for (size_t i = 0; i < h; i++)
+  for (int i = 0; i < h; i++)
   {
-    for (size_t j = 0; j < w; j++)
+    for (int j = 0; j < w; j++)
     {
-      rx = (i + 1 < h ? pixels[idx(i + 1, j, w)].r : 0) - (i - 1 >= 0 ? pixels[idx(i - 1, j, w)].r : 0);
-      gx = (i + 1 < h ? pixels[idx(i + 1, j, w)].g : 0) - (i - 1 >= 0 ? pixels[idx(i - 1, j, w)].g : 0);
-      bx = (i + 1 < h ? pixels[idx(i + 1, j, w)].b : 0) - (i - 1 >= 0 ? pixels[idx(i - 1, j, w)].b : 0);
-      ry = (j + 1 < w ? pixels[idx(i, j + 1, w)].r : 0) - (j - 1 >= 0 ? pixels[idx(i, j - 1, w)].r : 0);
-      gy = (j + 1 < w ? pixels[idx(i, j + 1, w)].g : 0) - (j - 1 >= 0 ? pixels[idx(i, j - 1, w)].g : 0);
-      by = (j + 1 < w ? pixels[idx(i, j + 1, w)].b : 0) - (j - 1 >= 0 ? pixels[idx(i, j - 1, w)].b : 0);
+      ry = (i + 1 < h ? pixels[idx(i + 1, j, w)].r : 0) - (i - 1 >= 0 ? pixels[idx(i - 1, j, w)].r : 0);
+      gy = (i + 1 < h ? pixels[idx(i + 1, j, w)].g : 0) - (i - 1 >= 0 ? pixels[idx(i - 1, j, w)].g : 0);
+      by = (i + 1 < h ? pixels[idx(i + 1, j, w)].b : 0) - (i - 1 >= 0 ? pixels[idx(i - 1, j, w)].b : 0);
+      rx = (j + 1 < w ? pixels[idx(i, j + 1, w)].r : 0) - (j - 1 >= 0 ? pixels[idx(i, j - 1, w)].r : 0);
+      gx = (j + 1 < w ? pixels[idx(i, j + 1, w)].g : 0) - (j - 1 >= 0 ? pixels[idx(i, j - 1, w)].g : 0);
+      bx = (j + 1 < w ? pixels[idx(i, j + 1, w)].b : 0) - (j - 1 >= 0 ? pixels[idx(i, j - 1, w)].b : 0);
+
       dx = (pow(rx, 2) + pow(gx, 2) + pow(bx, 2));
       dy = (pow(ry, 2) + pow(gy, 2) + pow(by, 2));
       energies[idx(i, j, w)] = sqrt(dx + dy);
@@ -208,73 +230,3 @@ size_t idx(size_t row, size_t col, int w)
 {
   return row * w + col;
 }
-
-// int minDistance(int dist[], bool sptSet[])
-// {
-//   // Initialize min value
-//   int min = INT_MAX, min_index;
-
-//   for (int v = 0; v < V; v++)
-//     if (sptSet[v] == false && dist[v] <= min)
-//       min = dist[v], min_index = v;
-
-//   return min_index;
-// }
-
-// // A utility function to print the constructed distance array
-// int printSolution(int dist[], int n)
-// {
-//   printf("Vertex   Distance from Source\n");
-//   for (int i = 0; i < V; i++)
-//     printf("%d tt %d\n", i, dist[i]);
-// }
-
-// // Function that implements Dijkstra's single source shortest path algorithm
-// // for a graph represented using adjacency matrix representation
-// void dijkstra(int *energy, int src, int w, int h)
-// {
-//   int dist[w * h]; // The output array.  dist[i] will hold the shortest
-//   // distance from src to i
-
-//   bool sptSet[w * h]; // sptSet[i] will be true if vertex i is included in shortest
-//   // path tree or shortest distance from src to i is finalized
-
-//   // Initialize all distances as INFINITE and stpSet[] as false
-//   for (int i = 0; i < w * h; i++)
-//     dist[i] = INT_MAX, sptSet[i] = false;
-
-//   // Distance of source vertex from itself is always 0
-//   dist[src] = 0;
-
-//   // Find shortest path for all vertices
-//   for (int count = 0; count < w * h - 1; count++)
-//   {
-//     // Pick the minimum distance vertex from the set of vertices not
-//     // yet processed. u is always equal to src in the first iteration.
-//     int u = minDistance(dist, sptSet);
-
-//     // Mark the picked vertex as processed
-//     sptSet[u] = true;
-
-//     // Update dist value of the adjacent vertices of the picked vertex.
-//     for (int v = 0; v < w * h; v++)
-
-//       // Update dist[v] only if is not in sptSet, there is an edge from
-//       // u to v, and total weight of path from src to  v through u is
-//       // smaller than current value of dist[v]
-//       if (!sptSet[v] && energy[idx(u, v, w)] && dist[u] != INT_MAX && dist[u] + energy[idx(u, v, w)] < dist[v])
-//         dist[v] = dist[u] + energy[idx(u, v, w)];
-//   }
-
-//   // print the constructed distance array
-//   printSolution(dist, w * h);
-// }
-
-// void find_curve_vertical(float *energies, int w, int h, pixel3_t *energy_image)
-// {
-//   bool sptSet[w * h];
-// }
-
-// void find_curve_horizontal(float *energies, int w, int h, pixel3_t *energy_image)
-// {
-// }
